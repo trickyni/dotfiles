e1819122ab0b0619ed5d21ee60ea649707672be9
trickyni <trickyniv56@gmail.com>
2026-01-05 15:30:10 +0200

    0

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -1,6 +1,6 @@
-require("starship"):setup({
-  config_file = "~/.config/starship/starship_oneline.toml",
-})
+-- require("starship"):setup({
+--   config_file = "~/.config/starship/starship_oneline.toml",
+-- })
 
 -- require("no-status"):setup()
 
