fd58deeee4cba5c18c0453d56f6b2dbc0c34645f
trickyni <trickyniv56@gmail.com>
2026-01-25 15:06:51 +0200

    0

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -2,11 +2,5 @@ require("starship"):setup({
   config_file = "~/.config/starship/starship_oneline.toml",
 })
 
--- require("no-status"):setup()
-
--- require("restore"):setup({
---   position = { "center", w = 70, h = 40 },
---   show_confirm = true,
--- })
 require("recycle-bin"):setup()
 require("relative-motions"):setup({ show_numbers = "relative_absolute" })
